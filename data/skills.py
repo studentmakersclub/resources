8e12a0a8655419227fcccf0c6ae17b5ea5308dba
Avi Aryan <avi.aryan123@gmail.com>
2018-09-26 13:23:59 +0530

    Create skills.py

---
+++ data/skills.py
@@ -0,0 +1,16 @@
+# List of skills
+
+SKILLS = {
+	'js': {
+		'name': 'JavaScript',
+	},
+	'python': {
+		'name': 'Python',
+	},
+	'java': {
+		'name': 'Java',
+	},
+	'jquery': {
+		'name': 'jQuery'
+	}
+}
